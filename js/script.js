1a4c275bbd0c734fe9a7d429cd3d08ebcb52cb4d
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-08-08 14:22:08 +0300

    version 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 //task 1
 function arrSum(inputArr) {
     let result = 0;
-    for (let i=0; i<inputArr.length; i++) {
-        result += parseFloat(inputArr[i]);
-    }
+    for (let i=0; i<inputArr.length; i++) result += parseFloat(inputArr[i]);
     return result;
+
     // для случая если надо собрать все символы в одну строку
     // let result = "";
     // for (let i=0; i<inputArr.length; i++) {
@@ -13,11 +12,18 @@ function arrSum(inputArr) {
     // return result;
 }
 //task 2
-function searchValue(inputArr, value) {
-    for (let i=0; i<inputArr.length; i++) {
-        if (inputArr[i] == value) return i;
+function searchValue(inArr, inValue) {
+    let outArr = [];
+    for (let i=0, n=-1; i<inArr.length; i++) {
+        console.log(inArr[i]);
+        console.log(inValue[0]);
+        if (inArr[i] == inValue[0]) {
+            n++;
+            outArr[n] = i;
+        }
     }
-    return -1;
+    if (outArr.length == 0) return -1;
+    else return outArr;
 }
 //task 3
 function addArr(arrFirst, arrSecond){
@@ -28,31 +34,26 @@ function addArr(arrFirst, arrSecond){
 }
 
 function parseArr(inValues) {
-    console.log(inValues);
     let temp = inValues;
+    console.log(temp);
     let arr = [];
     let element = "";
-    let f = 0;
-    for (let i=0, newArr=0; ; i++) {
+    let f = 1;
+
+    for (let i=0, newArr=-1; ; i++) {
         let symbol = String(temp.substr(i, 1));
-        //element += symbol === " " ? "" : symbol;
-
-        if (symbol && (symbol != " ")) f = 1;
-        console.log("flag");
-
-        if ((symbol === " ") || (symbol === "") && (f == 1)) {
+        if (symbol === " " && f === 1) continue;
+        else if (symbol !== " " && symbol !== "") {
+            element += symbol;
             f = 0;
-            arr[newArr] = element;
-            console.log(element);
-            newArr++;
+        } else if (symbol === " " && element != "") {
+            f = 1;
+            arr[++newArr] = element;
             element = "";
-        }
-        element += f == 1 ? symbol : "";
-        if (symbol === "") {
-            return arr;
-            brake;
-        }
+        } else if (symbol === "") break;
     }
+    console.log(arr);
+    return arr;
 }
 function arrSumEntered() {
     let enteredArr = prompt("Введите массив элементов (элементы разделять проблелом)");
@@ -66,14 +67,15 @@ function arrSumEntered() {
 function searchEntered() {
     let enteredArr = prompt("Введите массив элементов (элементы разделять проблелом)");
     let enteredValue = prompt("Введите искомое значение");
-    let arr = parseArr(enteredArr);
+    let array = parseArr(enteredArr);
+    let value = parseArr(enteredValue)
     let  inEnteredArr = document.querySelector(".search-arr-in");
-    inEnteredArr.innerHTML = arr;
+    inEnteredArr.innerHTML = array;
     let  inEnteredValue = document.querySelector(".search-value-in");
     inEnteredValue.innerHTML = enteredValue;
 
     let  searchOut = document.querySelector(".search-out");
-    searchOut.innerHTML = searchValue(arr, enteredValue);
+    searchOut.innerHTML = searchValue(array, value);
 }
 function addArrEntered() {
     let enteredArrFirst = prompt("Введите первый массив элементов (элементы разделять проблелом)");
@@ -89,6 +91,8 @@ function addArrEntered() {
     let  outArr = document.querySelector(".addArr-out");
     outArr.innerHTML = addArr(arrFirstParse, arrSecondParse);
 }
+
+
 
 
 
